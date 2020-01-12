4250d440fbcb73e3c54bcf5f52d3c20e8fb8f868
h5p9sl <21267024+h5p9sl@users.noreply.github.com>
2020-01-11 19:42:53 -0700

    clean up event loop

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,26 @@
-use backend::*;
 use piston_window::*;
+
+use ::backend::*;
 
 fn main() {
     println!("Hello, world!");
     let mut window = RenderWindow::new("SkyFall", [800, 600], false);
 
-    let circle = CircleShape::new(128.);
+    let circle = CircleShape::new(256.);
+    //let button = ::backend::user_interface::Button::new("Click me!");
 
-    loop {
-        while let Some(e) = window.poll_event() {
-            if let Some(args) = e.close_args() {
-                return;
-            }
-            if let Some(args) = e.render_args() {
-                window.clear([0., 0., 0., 1.]);
-                window.draw(&circle);
-                window.display(&args);
-            }
-            if let Some(args) = e.update_args() {
-                println!("Delta: {}", args.dt);
-            }
+    while let Some(e) = window.poll_event() {
+        if let Some(args) = e.close_args() {
+            return;
+        }
+        if let Some(args) = e.render_args() {
+            window.clear([0., 0., 0., 1.]);
+            window.draw(&circle);
+            window.draw(&button);
+            window.display(&args);
+        }
+        if let Some(args) = e.update_args() {
+            println!("Delta: {}", args.dt);
         }
     }
 }
