a1e8c87542b5d285a155b2130eb5dba32cf18a56
h5p9sl <21267024+h5p9sl@users.noreply.github.com>
2020-01-15 19:11:34 -0700

    Add exit button

--- src/main_menu.rs
+++ src/main_menu.rs
@@ -3,6 +3,7 @@ use piston::Input;
 
 pub struct MainMenu {
     start_button: GuiButton<'static>,
+    exit_button: GuiButton<'static>
 }
 
 impl MainMenu {
@@ -12,16 +13,24 @@ impl MainMenu {
             start_button: GuiButton::new("Start game")
                 .size((300., 50.))
                 .position((250., 350.)),
+            exit_button: GuiButton::new("Exit game")
+                .size((300., 50.))
+                .position((250., 405.)),
         }
     }
 
     pub fn on_input(&mut self, input: &Input) {
         self.start_button.on_input(input);
+        if self.exit_button.on_input(input) {
+            // Lazy exit
+            std::process::exit(0);
+        }
     }
 
     pub fn update(&mut self, delta: f64) {}
 
     pub fn draw(&mut self, window: &mut RenderWindow) {
         window.draw(&mut self.start_button);
+        window.draw(&mut self.exit_button);
     }
 }
