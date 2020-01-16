fcf5bc8fba33c82f81a239b9568fff85004959c0
h5p9sl <21267024+h5p9sl@users.noreply.github.com>
2020-01-15 19:23:31 -0700

    Add temp. title for Main Menu

--- src/main_menu.rs
+++ src/main_menu.rs
@@ -1,15 +1,22 @@
-use ::backend::{GuiButton, RenderWindow};
+use ::backend::{GuiButton, GuiLabel, RenderWindow};
 use piston::Input;
 
 pub struct MainMenu {
+    title: GuiLabel<'static>,
     start_button: GuiButton<'static>,
-    exit_button: GuiButton<'static>
+    exit_button: GuiButton<'static>,
 }
 
 impl MainMenu {
     pub fn new() -> MainMenu {
         println!("Initializing MainMenu");
         MainMenu {
+            title: GuiLabel::new("SkyFall Rust")
+                .font_size(48)
+                .color([1.0, 1.0, 1.0, 1.0])
+                .origin((0.5, 0.5))
+                .position((400., 200.)),
+
             start_button: GuiButton::new("Start game")
                 .size((300., 50.))
                 .position((250., 350.)),
@@ -32,5 +39,6 @@ impl MainMenu {
     pub fn draw(&mut self, window: &mut RenderWindow) {
         window.draw(&mut self.start_button);
         window.draw(&mut self.exit_button);
+        window.draw(&mut self.title);
     }
 }
